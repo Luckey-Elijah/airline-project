b69dba3b8ea165d3d7e0e222e063d021cff965bc
Luckey-Elijah <38171761+Luckey-Elijah@users.noreply.github.com>
2018-12-02 21:41:27 -0500

    Add files via upload

--- src/windowFXMLController.java
+++ src/windowFXMLController.java
@@ -5,8 +5,9 @@
 
 package airlinetoolapp;
 
-import java.io.FileInputStream;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
@@ -25,29 +26,98 @@ public class windowFXMLController implements Initializable {
     private TextField flightNumField, flightDateField, departureTimeField, 
             arrivalTimeField, departureCityField, arrivalCityField, 
             availableSeatsField, flightNumberRes,  PassengerIDRes, 
-            passengerNameRes, seatNumberRes;
-
+            passengerNameRes, seatNumberRes, flightNumberOnMapField, 
+            SpecificFlightTextField;
     
     @FXML
     private Pane newFlightPane, newReservationPane, seatMapPane, 
             listAllFlightsPane, listAllReservationsPane, 
             listAllFlightReservationsPane, exitPane;
     
+    @FXML
+    private TextArea seatMapArea, listAllFlightsArea, listOfAllPassangerReservations, 
+    listOfSpecificPassangerReservations;
     
     @FXML
-    private TextArea seatMapArea, listAllFlightsArea;
-
-    
-    @FXML
-    void updatSeatMap(ActionEvent event) {
+    void updatSeatMap(ActionEvent event) throws FileNotFoundException, IOException {
         
-    }
-    
+        String flightNumberOnMap = flightNumberOnMapField.getText();
+        BufferedReader flightIn = null;
+        
+        seatMapArea.setText("");
+        
+        // used for creating file location - found in smae packagae as cwd
+        final String dir = System.getProperty("user.dir");
+        String packageString = "\\src\\airlinetoolapp\\";
+        String fileName = "flights.txt";
+        String fileNameFlights = dir + packageString + fileName;
+        
+        try{
+            
+            // using a Scanner object for "scanning"
+            flightIn = new BufferedReader(new FileReader(fileNameFlights));
+            String flightLine; // String use to display the whole line from .txt
+            String[] lineArr; // initialized array
+            
+            while ( (flightLine = flightIn.readLine()) != null) {
+                
+                // splits the line into an array of the values 
+                lineArr = flightLine.split("\t");
+                
+                // this location in the array will always hold the availble seats
+                int arrLength = lineArr.length-1; 
+
+                // the first [0] index in the array will always hold the flight#
+                if(lineArr[0].equals(flightNumberOnMap)){
+                    
+                    int numSeats    = Integer.parseInt(lineArr[arrLength]);
+                    int rowSeats    = numSeats / 7;
+                    int lastRowSeat = numSeats % 7;
+                    
+                    String mapSkel = "\tA B     C D E     F G\n";
+                    System.out.println(rowSeats + " " + lastRowSeat);
+                    
+                    // prints the rows onto the display
+                    for (int i = 0; i < rowSeats; i++) {
+                        seatMapArea.appendText((i+1) + mapSkel);
+                    }
+                    
+                    if (lastRowSeat > 0){
+                        switch (lastRowSeat){
+                            case 1: seatMapArea.appendText((rowSeats + 1) 
+                                    + "\tA\n");
+                            break;
+                            case 2: seatMapArea.appendText((rowSeats + 1) 
+                                    + "\tA B\n");
+                            break;
+                            case 3: seatMapArea.appendText((rowSeats + 1) 
+                                    + "\tA B     C\n");
+                            break;
+                            case 4: seatMapArea.appendText((rowSeats + 1) 
+                                    + "\tA B     C D\n");
+                            break;
+                            case 5: seatMapArea.appendText((rowSeats + 1) 
+                                    + "\tA B     C D E\n");
+                            break;
+                            case 6: seatMapArea.appendText((rowSeats + 1) 
+                                    + "\tA B     C D E    F\n");
+                            break;
+                        }
+                    }
+                    
+                }
+            }
+        }finally{
+            flightIn.close();
+        } 
+    } 
+
+    // addFlight() method adds the fields in the 
     @FXML
     void addFlight(ActionEvent event) throws FileNotFoundException, IOException {
-        FileInputStream flightsIn   = null;
-        FileWriter flightsOut       = null;
+        FileWriter flightsOut   = null;
         
+        // setup of String values
         String flightNumber     = flightNumField.getText();
         String flightDate       = flightDateField.getText();
         String departureTime    = departureTimeField.getText();
@@ -57,40 +127,47 @@ public class windowFXMLController implements Initializable {
         String availableSeats   = availableSeatsField.getText();
         
         final String dir = System.getProperty("user.dir");
-        String packageString = "\\airlinetoolapp\\";
-        String fileNameFlights = "flight.txt";
-        System.out.println(dir + packageString + fileNameFlights);
-        
+        String packageString = "\\src\\airlinetoolapp\\";
+        String fileName = "flights.txt";
+        String fileNameFlights = dir + packageString + fileName; // use print line to check
+
         try{
-            // flightsIn = new FileInputStream(dir + "");
-            // flightsOut = new FileWriter(dir + "");
+            // object assigned as a FileWrite object
+            flightsOut = new FileWriter(fileNameFlights, true);
             
-            System.out.println("current dir = " + dir);
-            
-            int c;
-            while ((c = flightsIn.read()) != -1){
-                System.out.print(c);
-                flightsOut.write(c);
-            }
+            // string for writing the data to the file
+            String flightDataLine = flightNumber + "\t" + flightDate  + "\t" + 
+                    departureTime + "\t" + arrivalTime + "\t" + departureCity + 
+                    "\t" + arrivalCity + "\t" + availableSeats + "\n";
+            flightsOut.write(flightDataLine); // data is written to file
         }finally{
-            if(flightsIn != null){
-                flightsIn.close();
-            }
-            if(flightsOut != null){
-                flightsOut.close();
-            }
+            flightsOut.close();
+            
         }
     }
     
     @FXML
-    void addNewReservation(ActionEvent event) {
+    void addNewReservation(ActionEvent event) throws FileNotFoundException, IOException {
         // on button press, method should read all text fields and then add to 
         // appropiate .txt files
         
+        // these create a String with proper file location
+        final String dir        = System.getProperty("user.dir");
+        String packageString    = "\\src\\airlinetoolapp\\";
+        String fileName         = "reservations.txt";
+        String fileNameFlights  = dir + packageString + fileName;
+        
+        // setup of String-grabbing for file writing
         String flightNumber     = flightNumberRes.getText();
         String passengerID      = PassengerIDRes.getText();
         String PassengerName    = passengerNameRes.getText();
         String seatNumber       = seatNumberRes.getText();
+        
+        try (FileWriter reservationsOut = new FileWriter(fileNameFlights, true)) {
+            
+            reservationsOut.write(passengerID + "\t" + PassengerName + "\t" +
+                    seatNumber + "\t" + flightNumber + "\n");
+        }
     }
     
     @FXML
@@ -109,13 +186,44 @@ public class windowFXMLController implements Initializable {
     }
 
     @FXML
-    void listAllFlightsButtonAction(ActionEvent event) {
+    void listAllFlightsButtonAction(ActionEvent event) throws FileNotFoundException, IOException {
         listAllFlightsPane.toFront();   // brings this pane to front at button press
+
+        final String dir = System.getProperty("user.dir");
+        String packageString = "\\src\\airlinetoolapp\\";
+        String fileNameFlights = "flights.txt";
+        
+        listAllFlightsArea.setText("");
+        BufferedReader bf = null;
+        try{
+            bf = new BufferedReader(new FileReader(dir+packageString+fileNameFlights));
+            String str;
+            while ((str = bf.readLine()) != null){
+                listAllFlightsArea.appendText(str+"\n");
+            }
+        }
+        finally{
+            bf.close();
+        }
     }
 
     @FXML
-    void listAllReservationsButtonAction(ActionEvent event) {
+    void listAllReservationsButtonAction(ActionEvent event) throws IOException {
+        
         listAllReservationsPane.toFront();  // brings this pane to front at button press
+        
+        final String dir = System.getProperty("user.dir");
+        String packageString = "\\src\\airlinetoolapp\\";
+        String fileNameFlights = "reservations.txt";
+        
+                
+        listOfAllPassangerReservations.setText("");
+        try(BufferedReader bf = new BufferedReader(new FileReader(dir+packageString+fileNameFlights))) {
+            String str;
+            while ((str = bf.readLine()) != null){
+                listOfAllPassangerReservations.appendText(str+"\n");
+            }
+        }
     }
 
     @FXML
@@ -133,10 +241,44 @@ public class windowFXMLController implements Initializable {
         Platform.exit();    // when exitApplication button is pressed, the application exits
     }
 
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-    }    
+    }
+
+    @FXML
+    void showSpecificFlightReservations(ActionEvent event) throws FileNotFoundException, IOException {
+        String flightNumber = SpecificFlightTextField.getText();
+        final String dir = System.getProperty("user.dir");
+        String packageString = "\\src\\airlinetoolapp\\";
+        String fileNameFlights = "reservations.txt";
+        
+                
+        listOfSpecificPassangerReservations.setText("");
+	listOfSpecificPassangerReservations.appendText(String.format("%-25s %-20s %-15s %-10s%n", "ID", "Name", "SeatNumber", "Flight#"));
+        BufferedReader bf = null;
+        try{
+            bf = new BufferedReader(new FileReader(dir+packageString+fileNameFlights));
+	    
+            String str;
+            while ((str = bf.readLine()) != null){
+                String[] strArray = str.split("\t");
+                //listOfSpecificPassangerReservations.setText(strArray[3]);
+		System.out.println(strArray[3]);
+		
+		//System.out.println(flightNumber);
+                if (flightNumber.contentEquals(strArray[3])){
+                    listOfSpecificPassangerReservations.appendText(String.format("%-20s %-20s %-15s %-10s%n", strArray[0], strArray[1], 
+			    strArray[2], strArray[3]));
+		    System.out.println("KK");
+                }
+            }
+        }
+        finally{
+            bf.close();
+        }
+        
+
+    }
     
 }
